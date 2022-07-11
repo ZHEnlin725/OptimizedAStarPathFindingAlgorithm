c6f11170be2d016748412a6dc9cdbb83ba8c23f5
zhenlin <zhenlin725@163.com>
2022-07-11 17:22:35 +0800

    no message

--- Assets/PathFinding/TriangleNavMesh/BSP/BSPTree.cs
+++ Assets/PathFinding/TriangleNavMesh/BSP/BSPTree.cs
@@ -9,16 +9,14 @@ namespace PathFinding.TriangleNavMesh.BSP
 
         private BSPNode _root;
 
-        private List<TriangleWrap> _triangleWraps;
-
         public void Init(IList<Triangle> triangles)
         {
             var count = triangles.Count;
-            _triangleWraps = new List<TriangleWrap>(count);
+            var triangleWraps = new List<TriangleWrap>(count);
             for (int i = 0; i < count; i++)
-                _triangleWraps.Add(new TriangleWrap(i, triangles[i].V0, triangles[i].V1, triangles[i].V2));
+                triangleWraps.Add(new TriangleWrap(i, triangles[i].V0, triangles[i].V1, triangles[i].V2));
             _root = new BSPNode();
-            _root.Init(this, _triangleWraps, 0);
+            _root.Init(this, triangleWraps, 0);
         }
 
         public int TriangleIndex(Vector3 pos) => _root.TriangleIndex(new Vector2(pos.x, pos.z));
